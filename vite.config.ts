5a1aa891d89e31eb4f9ebf83701132769aadfc1f
Ix12314 <jiangai9999@163.com>
2026-07-20 10:14:49 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  base: '/mc-newbie-guide/',
+  build: {
+    sourcemap: 'hidden',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: true,
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'react-dev-locator',
+        ],
+      },
+    }),
+    tsconfigPaths()
+  ],
+})
